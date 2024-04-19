8a9987cf8e5df08b47952e8551cabe7175a40bce
Richard Wanderrainy <wanderdev01@gmail.com>
2024-04-19 15:07:40 -0300

    Adding program

---
+++ forca.h
@@ -0,0 +1,12 @@
+#define tamanho_palavra 20
+#define tamanho_chute 20
+
+void abertura();
+void chuta (int* tent);
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+int acertou();
+int enforcou();
+void adicionapalavra();
+int chuteserrados();
